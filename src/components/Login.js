ee839fd3e325f7b63e9899f9e7e3ba4ab3427de7
Arpit1434 <arpit.tiwari1434@gmail.com>
2025-06-19 22:11:40 +0530

    Added Backend URLs

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,7 +12,7 @@ const Login = (props) => {
   const handleSubmit = async (e) => {
     e.preventDefault()
     const { email, password } = credentials
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch("https://notes-app-backend-zixf.onrender.com/api/auth/login", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
