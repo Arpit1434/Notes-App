f90fb2d64d4d123bdac73ba1b501b14c8d89f927
Arpit1434 <arpit.tiwari1434@gmail.com>
2025-06-26 19:22:08 +0530

    FIX 4: CORS error

--- backend/index.js
+++ backend/index.js
@@ -8,13 +8,12 @@ connectToMongo()
 const app = express()
 const port = process.env.PORT
 
-app.use(cors())
-app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, auth-token");
-   next();
-});
+app.use(cors({
+  'allowedHeaders': ['auth-token', 'Content-Type'],
+  'origin': '*',
+  'methods': 'GET,PUT,POST,DELETE',
+  'preflightContinue': false
+}));
 app.use(express.json())
 
 // Available routes
