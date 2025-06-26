a22c8b3abc7c9f0be610090b82afc0e5a5e539bd
Arpit1434 <arpit.tiwari1434@gmail.com>
2025-06-26 19:18:10 +0530

    FIX 3: CORS error

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,12 @@ const app = express()
 const port = process.env.PORT
 
 app.use(cors())
-app.options("*", cors())
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, auth-token");
+   next();
+});
 app.use(express.json())
 
 // Available routes
